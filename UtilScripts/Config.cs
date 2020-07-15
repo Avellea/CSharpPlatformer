07b0f65052bde79c240a87fdf64407e73a7f90b2
Dynamic Donkey <34009754+DynamicDonkey@users.noreply.github.com>
2020-07-15 02:18:29 -0500

    Added fullscreen checking. See comments in UtilScript/Config.cs

--- UtilScripts/Config.cs
+++ UtilScripts/Config.cs
@@ -5,16 +5,32 @@ public class Config {
 
     private ConfigFile CF = new ConfigFile();
     string lang;
+    string isFullscreen;
 
     public void Save() {
         CF.SetValue("Main", "Lang", TranslationServer.GetLocale());
+        CF.SetValue("Main", "FULLSCREEN_NOTE", "For some reason this is reversed. Remember that when changing it.");
+        CF.SetValue("Main", "Fullscreen", OS.WindowFullscreen);
         CF.Save("user://settings.cfg");
+    }
+
+    private void setFullscreen() {
+        isFullscreen = CF.GetValue("Main", "Fullscreen", "True").ToString();
+
+        if(isFullscreen == "True") {
+            OS.WindowFullscreen = false;
+        } else {
+            OS.WindowFullscreen = true;
+        }
     }
 
     public void Load() {
         //GD.Print(OS.GetUserDataDir()); //C:\Users\<USER>\AppData\Roaming\Godot\app_userdata\C# Platformer\settings.cfg
         lang = CF.GetValue("Main", "Lang", "en").ToString();
         TranslationServer.SetLocale(lang);
+        
+        setFullscreen();
+
     }
 
     public void CheckExist() {
--- UtilScripts/fullscreen.cs
+++ UtilScripts/fullscreen.cs
@@ -2,8 +2,11 @@ using Godot;
 
 public class fullscreen : Node {
 
+	Config config = new Config();
+
 	public override void _Process(float delta) {
 		if(Input.IsActionJustPressed("toggle_fullscreen")) {
+			config.Save();
 			OS.WindowFullscreen = !OS.WindowFullscreen;
 		}
 	}
